b6c4c499d0e7711204aa9454b63a86f6e1c6aa6f
Stefano Giometti <stefano.giometti@isobar.com>
2018-01-23 01:45:49 +0100

    First commit. Setting up environment

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+    context:            path.join(__dirname, 'src'),
+    entry: {
+        app:            './app.js'
+    },
+    output: {
+        filename:       './js/[name].bundle.js',
+        path:           path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: /src/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            // Esporta HTML come stringa,
+            // minimizzata quando il compiler lo richiede
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+
+    devServer: {
+        contentBase:    path.join(__dirname, 'dist/static/images'),
+        stats:          'errors-only',
+        open:           false,
+        port:           3002,
+        compress:       true
+    },
+    devtool:            'inline-source-map'
+}
+
+module.exports = config
